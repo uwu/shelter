c2f85ea172a6f40a4e1c19a0c5c37fca90ff6dc1
creatable <cr3atable@riseup.net>
2022-10-06 04:24:53 -0400

    add dispatcher, solid, and patcher to window object

---
+++ packages/shelter/src/getDispatcher.ts
@@ -0,0 +1,44 @@
+import { webpackChunk } from "@cumjar/websmack";
+
+declare global {
+  interface Object {
+    _dispatcher: undefined;
+  }
+}
+
+export default async function getDispatcher() { // TODO: Actually type the dispatcher
+  let dispatcher;
+
+  try {
+    dispatcher = Object.values(
+      webpackChunk("webpackChunkdiscord_app")[0]
+    ).find(
+      // @ts-expect-error We have a fallback, see below!
+      (x) => x?.exports?.default?._dispatcher
+      // @ts-expect-error See above! ^
+    ).exports.default._dispatcher;
+  } catch {
+    // I know what you're thinking.
+    // You're thinking "What the fuck? An error not being handled?!"
+    // Read below!!!
+  }
+
+  // Promises bubble up, this is fine.
+  if (!dispatcher) return new Promise((res) => {
+    // Are you happy now?
+    Object.defineProperty(Object.prototype, "_dispatcher", {
+      set(value) {
+        dispatcher = value;
+        if (!dispatcher) {
+          res(dispatcher);
+          delete Object.prototype._dispatcher;
+        }
+      },
+      get() {
+        return dispatcher;
+      },
+    });
+  });
+
+  return dispatcher;
+}
--- packages/shelter/src/index.ts
+++ packages/shelter/src/index.ts
@@ -1,1 +1,22 @@
-export default "Hello, World!";
+import getDispatcher from "./getDispatcher";
+import * as patcher from "spitroast";
+import * as solid from "solid-js";
+
+// We can move this somewhere else, I just put it here for now.
+function log(text) {
+  console.log("%cshelter%c", "background: linear-gradient(180deg, #2A3B4B 0%, #2BFAAC 343.17%); color: white; padding: 6px", "", text)
+}
+
+const start = performance.now()
+log("shelter is initializing...")
+
+getDispatcher().then(FluxDispatcher => {
+  // We can potentially move the window obj to it's own module later, I think it's help with typedefs?
+  window["shelter"] = {
+    FluxDispatcher,
+    patcher,
+    solid,
+  }
+
+  log(`shelter is initialized. took: ${(performance.now() - start).toFixed(1)}ms`)
+})
