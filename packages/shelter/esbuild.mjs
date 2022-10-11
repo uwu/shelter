098dd8d978dba9b6f7a88770cae8e9d8d3d27a71
Alyxia Sother <alyxia@riseup.net>
2022-10-11 09:15:40 +0200

    [Meta] Set up CSS modules
    WARNING: At the moment, there are no internal type definitions for
    *.scss files yet, so anything attempting to import a scss file will
    error. For now, ignore the error and continue working.

--- packages/shelter/esbuild.mjs
+++ packages/shelter/esbuild.mjs
@@ -1,18 +1,21 @@
 import { build } from "esbuild";
+import { postcssModules, sassPlugin } from "esbuild-sass-plugin";
 import { solidPlugin } from "esbuild-plugin-solid";
 
 build({
-	entryPoints: ["src/index.ts"],
-	bundle: true,
-	outfile: "dist/shelter.js",
-	sourcemap: "external",
-	minify: true,
-    loader: {
-        ".png": "dataurl",
-        ".jpg": "dataurl",
-        ".svg": "text",
-    },
-	plugins: [
-		solidPlugin()
-	]
-})
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  outfile: "dist/shelter.js",
+  sourcemap: "external",
+  minify: true,
+  loader: {
+    ".png": "dataurl",
+    ".jpg": "dataurl",
+    ".svg": "text",
+  },
+  plugins: [
+    solidPlugin(),
+    sassPlugin({ style: "compressed", transform: postcssModules({ localsConvention: "camelCaseOnly" }) }),
+  ],
+});
+
