6793fa3d98ea050a555954fac607b21e99098511
Yellowsink <yellowsink@riseup.net>
2024-01-24 19:33:58 +0000

    [injectors > kernel] Remove CSP, overkill edition.

--- injectors/kernel/main.js
+++ injectors/kernel/main.js
@@ -1,15 +1,72 @@
-const { ipcMain } = require("electron");
+const { ipcMain, app, session } = require("electron");
 const { promises: fs } = require("original-fs");
 const https = require("https");
 const path = require("path");
 const config = require("./index.json");
 
+// completely disable CSP
+console.log("[kernel-shelter] Disabling CSP");
+// other injectors might get to onHeadersReceived first and modify it, so to prevent that we'll patch over it
+// before the app is even ready.
+// sorry mod authors, I WILL win the CSP modification fight against you, just watch me.
+
+const origGetter = Object.getOwnPropertyDescriptor(session, "defaultSession").get;
+
+delete session.defaultSession;
+let cspIsPatched = false;
+Object.defineProperty(session, "defaultSession", {
+  enumerable: true,
+  configurable: false, // remember when I said I WILL win?
+  get() {
+    const dsess = origGetter();
+    console.log("[kernel-shelter] Intercepted electron.session.defaultSession getter.");
+
+    // if we are here, then app must be ready, as this getter throws when not ready
+    // this means it's safe to patch CSP now without doing any further waiting
+
+    if (!cspIsPatched) {
+      dsess.webRequest.onHeadersReceived(({ responseHeaders }, done) => {
+        const cspHeaders = Object.keys(responseHeaders).filter(
+          (name) =>
+            name.toLowerCase().startsWith("content-security-policy") ||
+            name.toLowerCase().startsWith("access-control-allow-origin"),
+        );
+
+        for (const header of cspHeaders) delete responseHeaders[header];
+
+        // Allow loading content from sites that don't allow CORS, like raw.githubusercontent.com
+        // Vencord's uses this for text/css sources only to allow this specific site
+        // This is their rationale for trying to stop other mods trampling theirs so if we not only fix it for us but
+        // fix it for them too, then I think we come to a pretty agreeable solution ;)
+        // We do, however, leave headers, methods, and other CORS headers intact, to prevent breakages.
+        responseHeaders["Access-Control-Allow-Origin"] = "*";
+
+        done({ responseHeaders });
+      });
+
+      // I promise, I tested leaving this as is to give other client mods the benefit of the doubt.
+      // With Vencord too, our callback doesn't get run, so to prevent it from being trampled, we'll just do it here.
+      dsess.webRequest.onHeadersReceived = () => {};
+
+      console.log("[kernel-shelter] Patched CSP before handing back to getter source.");
+      cspIsPatched = true;
+    }
+
+    return dsess;
+  },
+});
+
+// Call the getter just in case another mod isn't there to do it for us. This patches CSP in that case.
+app.on("ready", () => session.defaultSession);
+
+// read config
 const sourceType = config.settings?.sourceType || "remote";
 const sourcePath =
   config.settings?.sourcePath || "https://raw.githubusercontent.com/uwu/shelter-builds/main/shelter.js";
 
 console.log(`[kernel-shelter] Loading shelter from ${sourceType} "${sourcePath}"`);
 
+// fetch shelter
 let bundle;
 if (sourceType === "remote") {
   bundle = new Promise((resolve, reject) => {
@@ -38,4 +95,5 @@ if (sourceType === "remote") {
   bundle = fs.readFile(resolved, "utf8").then((v) => v + `\n//# sourceMappingURL=file://${resolved}.map`);
 }
 
+// handle injecting shelter
 ipcMain.handle("_shelter_getBundle", () => bundle);
