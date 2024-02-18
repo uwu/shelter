4b626755160ba1c88429f987e8db4d93dae73745
ioj4 <69911332+ioj4@users.noreply.github.com>
2024-02-18 15:58:47 +0100

    [defs] add typedefs for plugin scoped api

--- packages/shelter-defs/rootdefs.ts
+++ packages/shelter-defs/rootdefs.ts
@@ -1,11 +1,13 @@
 import { ShelterApi } from "shelter/src/windowApi";
+import { ShelterPluginApi } from "shelter/src/plugins";
 
 export { ShelterApi };
+export { ShelterPluginApi };
 
 export * from "shelter/src/types";
 
 declare global {
-  const shelter: ShelterApi;
+  const shelter: ShelterApi & { plugin: ShelterPluginApi };
 
   // noinspection JSUnusedGlobalSymbols
   interface Window {
