edc718436a43eee1300ba39c8c9776d33f59e207
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-02-19 23:24:57 +0530

    [docs] redesign plugins page (#20)
    * feat(docs): redesign plugins page
    
    * feat(docs): loading state, github repo
    
    * fix(docs): github icon twitching on copy
    
    * Revert "fix(docs): github icon twitching on copy"
    
    This reverts commit 158416333a0e5ff9c3a8904aad8c6379b9b485c2.
    
    * fix(docs): fix copy state
    
    * fix(docs): styles
    
    * fix broken filtering
    
    ---------
    
    Co-authored-by: Hazel Atkinson <yellowsink@riseup.net>

--- packages/shelter-docs/docs/.vitepress/theme/index.ts
+++ packages/shelter-docs/docs/.vitepress/theme/index.ts
@@ -1,2 +1,3 @@
 export { default } from "vitepress/theme";
+import "uno.css";
 import "./custom.css";
--- packages/shelter-docs/docs/components/PluginListing.vue
+++ packages/shelter-docs/docs/components/PluginListing.vue
@@ -1,34 +1,105 @@
 <script setup lang="ts">
-import { ref, reactive, watchEffect, computed } from "vue";
+import {ref, reactive, computed, onMounted} from "vue";
 import { useFuse } from "@vueuse/integrations/useFuse";
+import { useClipboard } from "@vueuse/core";
 
 interface PluginManifest {
-    name: string;
-    description: string;
-    author: string;
-    url: string;
+  name: string;
+  description: string;
+  author: string;
+  url: string;
+  repoName: string;
 }
 
 const data: PluginManifest[] = reactive([]);
-fetch("https://shindex.uwu.network/data").then((r) => r.json()).then((j) => data.push(...j.flatMap((r) => r.plugins)));
+let isLoading = ref(true);
+
+fetch("https://shindex.uwu.network/data")
+  .then((r) => r.json())
+  .then((items) => {
+    items.forEach((item) => {
+      item.plugins.forEach((plugin: PluginManifest) => data.push({
+        name: plugin.name,
+        description: plugin.description,
+        author: plugin.author,
+        url: plugin.url,
+        repoName: item.name
+      }))
+    });
+  })
+  .then(() => (isLoading.value = false));
+
 const search = ref("");
 
 const { results } = useFuse(search, data, {
-    fuseOptions: {
-        keys: ["name", "description"],
-        threshold: 0.5,
-        useExtendedSearch: true,
-    },
+  fuseOptions: {
+    keys: ["name", "description"],
+    threshold: 0.5,
+    useExtendedSearch: true,
+  },
 });
 
-const plugins = computed(() => results.value.length ? results.value.map((i) => i.item) : data);
+const { copy } = useClipboard();
+const isCopied = ref(new Array(data.length).fill(false));
+
+const copyUrl = (url: string, idx: number) => {
+  copy(url);
+  isCopied.value[idx] = true;
+  setTimeout(() => {
+    resetCopyState();
+  }, 1500);
+};
+
+const resetCopyState = () => {
+  isCopied.value.fill(false);
+};
+
+onMounted(() => {
+  resetCopyState();
+});
+
+const plugins = computed(() => (results.value.length ? results.value.map((i) => i.item) : data));
 </script>
 
 <template>
-    <input type="text" placeholder="Search..." v-model="search" />
-    <div v-for="plugin in plugins">
-        <h2>{{ plugin.name }}</h2>
-        <p>{{ plugin.description }}</p>
-        <code>{{ plugin.url }}</code>
+  <div h="1px" bg="$vp-c-divider" m="t-4" />
+  <div flex="~" class="children:my-auto" p="2">
+    <i i-carbon-search m="r-2" opacity="50" />
+    <input class="w-full" v-model="search" type="text" role="search" placeholder="Search..." />
+  </div>
+  <div h="1px" bg="$vp-c-divider" m="b-4" />
+
+  <div text-center v-if="isLoading">Loading plugins...</div>
+  <div v-else flex="~ wrap" gap-3 items-center>
+    <div v-for="(item, idx) in plugins" :key="idx" w-20rem h-42 px-4 py-3 border="1 solid $vp-c-divider" rounded-md
+      important-transition-all duration-400 hover="shadow-md bg-$vp-c-bg-soft" flex="~ col" justify-between>
+      <div font-semibold dark="text-gray-200" text-gray-900 text-16px>
+        {{ item.name }}
+      </div>
+
+      <div dark="text-gray-400" text-gray-500 text-14px>by {{ item.author }}</div>
+
+      <div text-gray-500 dark="text-gray-400" flex-auto mt-2 text-14px>
+        <span line-clamp-2>
+          {{ item.description }}
+        </span>
+      </div>
+
+      <div flex gap-5 items-end>
+        <div flex items-center gap-1>
+          <button @click="copyUrl(item.url, idx)" w-32 inline-flex justify-center whitespace-nowrap text-sm font-medium
+            cursor-pointer bg="$vp-badge-tip-bg" text="$vp-badge-tip-text" px2 py2 rounded-md block mt2 flex
+            items-center gap2>
+            <span v-if="!isCopied[idx]">Copy Plugin Link</span>
+            <span v-else>Copied!</span>
+          </button>
+        </div>
+        <div flex items-center gap-6 justify-right>
+          <a :href="`https://github.com/` + item.repoName" i-carbon-logo-github w-8 h-8 bg-dark dark:bg-light right-a
+            justify-right px2 ml-28 mt-2 flex items-center>
+          </a>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
