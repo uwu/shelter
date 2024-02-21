6cf5b624c9fd5d5490a00552b232e1cc31f2d9f5
ioj4 <69911332+ioj4@users.noreply.github.com>
2024-02-21 20:58:39 +0100

    [injectors/mv3] use csp workaround

---
+++ injectors/mv3/shelter-loader.js
@@ -0,0 +1,27 @@
+const SHELTER_URL = "https://raw.githubusercontent.com/uwu/shelter-builds/main/shelter.js";
+
+// discord removes localStorage later, so hold onto it
+const localStorage = window.localStorage;
+
+async function fetchShelter() {
+  const shelter = await (await fetch(SHELTER_URL)).text();
+  return `${shelter}\n//# sourceMappingURL=${SHELTER_URL}.map`;
+}
+
+async function initShelter() {
+  const localShelter = localStorage.getItem("shelter-bundle");
+
+  if (localShelter) {
+    const script = document.createElement("script");
+    script.innerText = localShelter;
+    document.head.append(script);
+  }
+
+  const remoteShelter = await fetchShelter();
+  if (remoteShelter === localShelter) return;
+
+  localStorage.setItem("shelter-bundle", remoteShelter);
+  window.location.reload();
+}
+
+initShelter();
