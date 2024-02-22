e7ea19459e98803ad4dfb7f76516b947f22dfa11
ioj4 <69911332+ioj4@users.noreply.github.com>
2024-02-22 15:17:09 +0100

    [injectors/mv3] rewrite with csp workaround (#22)
    * [injectors/mv3] use csp workaround
    
    * [injectors/mv3] append script to documentElement because head doesn't exist yet
    
    * [injectors/mv3] bump version to 1.2

--- injectors/mv3/content.js
+++ injectors/mv3/content.js
@@ -1,1 +1,4 @@
-chrome.runtime.sendMessage("pwnme");
+const script = document.createElement("script");
+script.src = chrome.runtime.getURL("shelter-loader.js");
+
+document.documentElement.append(script);
