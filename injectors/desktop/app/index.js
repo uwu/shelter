75298baa9ab8955b44e0124abac7f5c4290c2652
Hazel Atkinson <yellowsink@riseup.net>
2024-02-27 11:26:30 +0000

    Fully revert unnecessary changes

--- injectors/desktop/app/index.js
+++ injectors/desktop/app/index.js
@@ -46,9 +46,9 @@ const getShelterBundle = () =>
   !localBundle
     ? fetchPromise
     : Promise.resolve(
-      fs.readFileSync(path.join(localBundle, "shelter.js"), "utf8") +
-      `\n//# sourceMappingURL=file:////${path.join(localBundle, "shelter.js.map")}`,
-    );
+        fs.readFileSync(path.join(localBundle, "shelter.js"), "utf8") +
+          `\n//# sourceMappingURL=file:////${path.join(localBundle, "shelter.js.map")}`,
+      );
 // #endregion
 
 // #region IPC
