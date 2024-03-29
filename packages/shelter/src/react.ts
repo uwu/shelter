7fe0e8a5501beb44a4b6999c4015f06c29c98ee7
SpikeHD <spikegdofficial@gmail.com>
2024-03-29 11:13:03 -0700

    fix: exfiltrate React using useId

--- packages/shelter/src/react.ts
+++ packages/shelter/src/react.ts
@@ -2,5 +2,5 @@ import exfiltrate from "./exfiltrate";
 
 export let React, ReactDOM;
 
-exfiltrate("useRef").then((v) => (React = v));
+exfiltrate("useId").then((v) => (React = v));
 exfiltrate("findDOMNode").then((v) => (ReactDOM = v));
