597768e4d62a5dbea113d0e84d56f85da7cdb43a
ioj4 <69911332+ioj4@users.noreply.github.com>
2024-08-08 23:31:01 +0200

    refactor(index): fallback to native html attributes where available

--- packages/shelter-ui/src/index.tsx
+++ packages/shelter-ui/src/index.tsx
@@ -1,4 +1,4 @@
-import { Component, JSX } from "solid-js";
+import { Component, JSX, splitProps } from "solid-js";
 
 export * from "./util";
 export * from "./button";
@@ -16,27 +16,61 @@ export * from "./tooltip";
 export * from "./slider";
 export { InternalStyles, injectInternalStyles } from "./internalstyles";
 
-export const Text: Component<JSX.HTMLAttributes<HTMLSpanElement>> = (props) => (
+type TextProps = {
+  // overwritten to exclude plain string
+  style?: JSX.CSSProperties;
+};
+
+export const Text: Component<TextProps & Omit<JSX.HTMLAttributes<HTMLSpanElement>, keyof TextProps>> = (props) => (
   <span
+    {...props}
     style={{
       color: "var(--text-normal)",
+      ...props.style,
     }}
+  />
+);
+
+type DividerProps = {
+  mt?: boolean | string;
+  mb?: boolean | string;
+  // overwritten to exclude plain string
+  style?: JSX.CSSProperties;
+};
+
+export const Divider: Component<DividerProps & Omit<JSX.HTMLAttributes<HTMLDivElement>, keyof DividerProps>> = (
+  props,
+) => {
+  const [local, divProps] = splitProps(props, ["mt", "mb", "style"]);
+
+  return (
+    <div
+      // arguably this should be a <hr /> but UA styles beloved
+      role="separator"
+      {...divProps}
+      style={{
+        "margin-top": typeof local.mt === "string" ? local.mt : local.mt ? "20px" : "",
+        "margin-bottom": typeof local.mb === "string" ? local.mb : local.mb ? "20px" : "",
+        width: "100%",
+        height: "1px",
+        "border-top": "thin solid var(--background-modifier-accent)",
+        ...local.style,
+      }}
+    />
+  );
+};
+
+type SpaceProps = {
+  // overwritten to exclude plain string
+  style?: JSX.CSSProperties;
+};
+
+export const Space: Component<SpaceProps & Omit<JSX.HTMLAttributes<HTMLPreElement>, keyof SpaceProps>> = (props) => (
+  <pre
     {...props}
-  />
-);
-
-export const Divider: Component<{ mt?: boolean | string; mb?: boolean | string }> = (props) => (
-  <div
-    // arguably this should be a <hr /> but UA styles beloved
-    role="separator"
     style={{
-      "margin-top": typeof props.mt === "string" ? props.mt : props.mt ? "20px" : "",
-      "margin-bottom": typeof props.mb === "string" ? props.mb : props.mb ? "20px" : "",
-      width: "100%",
-      height: "1px",
-      "border-top": "thin solid var(--background-modifier-accent)",
+      display: "inline",
+      ...props.style,
     }}
   />
 );
-
-export const Space: Component = () => <pre style="display:inline"> </pre>;
