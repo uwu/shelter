df7699d680ef06b58a4eb748342c7cbd93b7a7f9
ioj4 <69911332+ioj4@users.noreply.github.com>
2024-11-08 02:22:53 +0100

    add kawaii banner

--- packages/shelter/src/components/Settings.tsx
+++ packages/shelter/src/components/Settings.tsx
@@ -1,6 +1,9 @@
-import { injectCss, Header, HeaderTags } from "@uwu/shelter-ui";
+import ShelterKawaiiSvg from "shelter-assets/svg/banner-kawaii.svg";
+import ShelterSvg from "shelter-assets/svg/banner.svg";
+import { injectCss, LinkButton } from "@uwu/shelter-ui";
 import { classes, css } from "./Settings.tsx.scss";
-import ShelterLogo from "./ShelterLogo";
+import { createSignal } from "solid-js";
+import { dbStore } from "../storage";
 import Plugins from "./Plugins";
 import DevUi from "./DevUI";
 
@@ -12,12 +15,31 @@ export default () => {
     injectedCss = true;
   }
 
+  dbStore.kawaiiBanner ??= false;
+  const [clicks, setClicks] = createSignal(0);
+
   return (
     <>
       <div class={classes.wrapper}>
-        <Header tag={HeaderTags.H1} class={classes.header}>
-          <ShelterLogo />- an attempt to prepare for the worst
-        </Header>
+        <div class={classes.header}>
+          <img
+            class={classes.banner}
+            onClick={() => {
+              if (clicks() < 5) {
+                setClicks(clicks() + 1);
+              } else {
+                setClicks(0);
+                dbStore.kawaiiBanner = !dbStore.kawaiiBanner;
+              }
+            }}
+            src={dbStore.kawaiiBanner ? ShelterKawaiiSvg : ShelterSvg}
+            draggable={false}
+          />
+          <div class={classes.linkwrapper}>
+            <LinkButton href="https://shelter.uwu.network/">Documentation</LinkButton>
+            <LinkButton href="https://github.com/uwu/shelter/">GitHub Repository</LinkButton>
+          </div>
+        </div>
         <DevUi />
         <Plugins />
       </div>
