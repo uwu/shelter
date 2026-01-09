59eaee52d246619d76f40cc0822a32e5e17da09f
ioj4 <69911332+ioj4@users.noreply.github.com>
2026-01-09 22:49:08 +0100

    [react] fix React exfiltration and add ReactDOMClient

--- packages/shelter/src/react.ts
+++ packages/shelter/src/react.ts
@@ -1,10 +1,19 @@
 import exfiltrate from "./exfiltrate";
 
-export let React, ReactDOM;
+export let React, ReactDOM, ReactDOMClient;
 
 // react developer tools' React is an experimental build, Discord's is not
 // we need to queuemicrotask the filter & cleanup for this as otherwise react does not actually set the version in time
 // RDT does not replace react-dom
 
-exfiltrate("useId", false, (r) => r?.version && !r.version.includes("experimental"), true).then((v) => (React = v));
+exfiltrate(
+  "useId",
+  false,
+  (r) => {
+    if (r?.version && !r.version?.includes?.("experimental")) React = r;
+    return false; // we want the last React that gets found
+  },
+  true,
+);
 exfiltrate("createPortal", false).then((v) => (ReactDOM = v));
+exfiltrate("createRoot", false).then((v) => (ReactDOMClient = v));
