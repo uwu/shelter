49a7fc78acbbea05fca83b1f0d69850b6a37790b
xirreal <me@xirreal.dev>
2026-02-04 16:16:32 +0100

    [shelter-ui] error boundary now shows error again and looks a bit nicer

--- packages/shelter-ui/src/errorboundary.tsx
+++ packages/shelter-ui/src/errorboundary.tsx
@@ -1,16 +1,20 @@
 import { ErrorBoundary as SEB } from "solid-js";
 import { Button, ButtonColors, ButtonLooks, ButtonSizes } from "./button";
+import { Header, HeaderTags } from "./header";
+import { TextArea } from "./textbox";
 import { css, classes } from "./errorboundary.tsx.scss";
 import { ensureInternalStyle } from "./internalstyles";
 
 const ErrBoundFallback = (err, reset) => {
   ensureInternalStyle(css);
 
+  console.error(err);
+
   return (
     <div class={classes.errbound}>
-      <h1 class={classes.errboundtitle}>Oops, we had a fucky wucky. (shelter)</h1>
-      <code class={classes.errboundcode}>{err}</code>
-      <Button color={ButtonColors.RED} size={ButtonSizes.TINY} look={ButtonLooks.FILLED} onClick={reset}>
+      <Header tag={HeaderTags.HeadingXL}>Oops, we had a fucky wucky. (shelter)</Header>
+      <TextArea value={err} mono readonly />
+      <Button color={ButtonColors.RED} size={ButtonSizes.SMALL} look={ButtonLooks.FILLED} onClick={reset}>
         Retry
       </Button>
     </div>
